70b65d1649b993732385bd15eb7bc8844a4d44ac
Sean Wildman <agoatnamedbear@gmail.com>
2024-06-24 17:46:15 -0400

    Add lastInputOperator variable to make sure that you cant have more than one operator at a time

--- app.js
+++ app.js
@@ -1,55 +1,104 @@
-const display = document.querySelector(".display");
-const numberButtons = document.querySelectorAll(".number-button");
-const operatorButtons = document.querySelectorAll(".operator");
-const equals = document.querySelector(".equals");
-const decimal = document.querySelector(".decimal");
-const allClear = document.querySelector(".all-clear");
-const plusMinus = document.querySelector(".plus-minus");
-const percentage = document.querySelector(".percentage");
-let firstNumber = "";
-let operator = "";
-let secondNumber = "";
+function calculator() {
+  const display = document.querySelector(".display");
+  const numberButtons = document.querySelectorAll(".number-button");
+  const operatorButtons = document.querySelectorAll(".operator");
+  const equals = document.querySelector(".equals");
+  const decimal = document.querySelector(".decimal");
+  const allClear = document.querySelector(".all-clear");
+  const plusMinus = document.querySelector(".plus-minus");
+  const percentage = document.querySelector(".percentage");
+  let firstNumber = "";
+  let operator = "";
+  let secondNumber = "";
+  let lastInputOperator = false;
 
-numberButtons.forEach((button) => {
-  button.addEventListener("click", () => {
-    const number = button.textContent;
-    inputNumber(number);
+  numberButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const number = button.textContent;
+      inputNumber(number);
+    });
   });
-});
 
-operatorButtons.forEach((operator) => {
-  operator.addEventListener("click", () => {
-    firstNumber = display.textContent;
-    display.textContent = firstNumber + operator.textContent;
+  operatorButtons.forEach((operatorButton) => {
+    operatorButton.addEventListener("click", () => {
+      if (lastInputOperator) {
+        operator = operatorButton.textContent;
+        display.textContent = operatorButton.textContent;
+      } else {
+        firstNumber = display.textContent;
+        operator = operatorButton.textContent;
+        display.textContent = operator;
+        lastInputOperator = true;
+        secondNumber = "";
+      }
+      console.log(`Operator: ${operator}`);
+    });
   });
-});
 
-allClear.addEventListener("click", () => {
-  if (display !== "") display.textContent = "";
-});
+  allClear.addEventListener("click", () => {
+    display.textContent = "";
+  });
 
-const inputNumber = (number) => {
-  const screenNumber = display.textContent;
-  if (screenNumber.length <= 10) {
-    display.innerHTML += number;
-  }
-};
+  const inputNumber = (number) => {
+    const screenNumber = display.textContent;
 
-const calculate = (firstNumber, operator, secondNumber) => {
-  switch (operator) {
-    case "+":
-      return firstNumber + secondNumber;
-    case "-":
-      return firstNumber - secondNumber;
-    case "*":
-      return firstNumber * secondNumber;
-    case "/":
-      return firstNumber / secondNumber;
-    default:
-      return "Invalid operator";
-  }
-};
+    if (lastInputOperator) {
+      display.textContent = "";
+      lastInputOperator = false;
+    }
 
-equals.addEventListener("click", () => {
-  calculate();
-});
+    if (screenNumber === "") {
+      display.textContent = number;
+      firstNumber = number;
+    } else {
+      if (screenNumber.length <= 10) {
+        display.textContent += number;
+        secondNumber = display.textContent;
+        console.log(`First Number: ${firstNumber}`);
+        console.log(`Second Number: ${secondNumber}`);
+      }
+    }
+  };
+
+  const calculate = (firstNumber, operator, secondNumber) => {
+    const num1 = parseFloat(firstNumber);
+    const num2 = parseFloat(secondNumber);
+
+    console.log(`Operator: ${operator}`);
+
+    switch (operator) {
+      case "+":
+        return num1 + num2;
+        break;
+      case "-":
+        return num1 - num2;
+        break;
+      case "*":
+        return num1 * num2;
+        break;
+      case "/":
+        return num1 / num2;
+        break;
+      default:
+        throw new Error("Invalid operator: " + operator);
+    }
+  };
+
+  equals.addEventListener("click", () => {
+    secondNumber = display.textContent;
+    if (firstNumber && secondNumber) {
+      console.log(`First Number: ${firstNumber}`);
+      console.log(`Second Number: ${secondNumber}`);
+      console.log(`Operator: ${operator}`);
+      const result = calculate(firstNumber, operator, secondNumber);
+      console.log(`Result: ${result}`);
+      display.textContent = result.toString();
+      firstNumber = "";
+      operator = "";
+      secondNumber = "";
+      lastInputOperator = false;
+    }
+  });
+}
+
+calculator();
